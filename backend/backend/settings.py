a334017c82eab94e1838ce080dd0497bcf1d7331
andrei <blalanuta@inbox.ru>
2020-11-06 00:22:01 +0100

    fixed database path

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -108,7 +108,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'sqlite3.db',  # Or path to database file if using sqlite3.
+        'NAME': str(BASE_DIR / 'db.sqlite3'),  # Or path to database file if using sqlite3.
         'USER': '',  # Not used with sqlite3.
         'PASSWORD': '',  # Not used with sqlite3.
         'HOST': '',  # Set to empty string for localhost. Not used with sqlite3.
