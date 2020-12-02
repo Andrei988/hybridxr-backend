e2146ef2549ec4213d8edf9fb933f839089d585a
andrei <blalanuta@inbox.ru>
2020-12-02 14:51:19 +0100

    changed location of static files

--- backend/backend/settings/common.py
+++ backend/backend/settings/common.py
@@ -103,10 +103,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(str(BASE_DIR), 'media')
+
+
+print(MEDIA_ROOT)
+print(MEDIA_ROOT)
+print(MEDIA_ROOT)
 MEDIA_URL = '/media/'
 
 EMAIL_BACKEND = config('EMAIL_BACKEND')
@@ -121,5 +125,3 @@ ALLOWED_HOSTS = [
     "snurfer98.pythonanywhere.com",
     '127.0.0.1'
 ]
-
-
