330eb7736619359ac98f84c2cd947c19d4e23c79
andrei <blalanuta@inbox.ru>
2020-12-05 19:02:46 +0100

    added assets

--- backend/backend/settings/settings.py
+++ backend/backend/settings/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 EMAIL_BACKEND = config('EMAIL_BACKEND')
